df69ff24477cb97b49005553bf81d83363529ced
code-with-Rinku <rinkusau2004@gmail.com>
2025-04-21 12:33:52 +0530

    Number_Guess.cpp

--- Number_Guess.cpp
+++ Number_Guess.cpp
@@ -1,73 +1,78 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
-void playGame(int min, int max, int attempts) {
-    int guess;
-    int secretNumber = rand() % (max - min + 1) + min;
-
-    cout << "\nYou have " << attempts << " attempts to guess the number between " << min << " and " << max << ". Good luck!\n";
-
-    for (int i = 1; i <= attempts; i++) {
-        cout << "\nAttempt " << i << ": Enter your guess: ";
-        cin >> guess;
-
-        if (guess == secretNumber) {
-            cout << " Well played! You guessed the number: " << guess << "\n";
-            cout << "\t\tThanks for playing!\n";
-            return;
-        } else {
-            cout << " Nope! " << guess << " is not the correct number.\n";
-            if (guess > secretNumber)
-                cout << "Hint: The secret number is smaller.\n";
-            else
-                cout << "Hint: The secret number is greater.\n";
-        }
-    }
-
-    cout << " Oops! You're out of attempts. The secret number was: " << secretNumber << "\n";
-    cout << "Play again if you want to try your luck!\n\n";
-}
-
 int main() {
-    int min, max, choice;
+    int min, max, guess;
 
-    cout << "\n\t\t Welcome to the Number Guessing Game! ??\n";
+    cout << "\n\t\t\tWelcome to the Number Guessing Game\n";
 
-    // Get range
+    // Ask for the range of the secret number
     cout << "\nEnter the minimum value: ";
     cin >> min;
     cout << "Enter the maximum value: ";
     cin >> max;
 
-    srand(time(0)); // Seed random generator
+    // Seed the random number generator
+    srand(time(0));
+
+    // Generate the secret number
+    int secretnumber = rand() % (max - min + 1) + min;
 
     while (true) {
-        cout << "\nChoose Difficulty Level:\n";
-        cout << "1. Easy   (10 attempts)\n";
-        cout << "2. Medium (7 attempts)\n";
-        cout << "3. Hard   (5 attempts)\n";
+        cout << "\nEnter the difficulty level:\n";
+        cout << "1. Easy\n";
+        cout << "2. Medium\n";
+        cout << "3. Hard\n";
         cout << "4. Exit\n";
+        cout << "You have limited chances based on the level you choose. Good luck!\n";
+
+        int choice;
         cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice) {
-            case 1:
-                playGame(min, max, 10);
+        int attempt = 0;
+        if (choice == 1) attempt = 10;
+        else if (choice == 2) attempt = 7;
+        else if (choice == 3) attempt = 5;
+        else if (choice == 4) exit(0);
+        else {
+            cout << "Wrong choice, enter a valid choice to play the game.\n";
+            continue;
+        }
+
+        cout << "You have " << attempt << " chances to find the secret number.\n";
+
+        for (int i = 0; i < attempt; i++) {
+            cout << "\nEnter your guess: ";
+            cin >> guess;
+
+            if (guess == secretnumber) {
+                cout << "Well played! You won with the number " << guess << "!\n";
+                cout << "\t\t\tThanks for playing!\n";
+                cout << "Play the game again with us if you want.\n";
                 break;
-            case 2:
-                playGame(min, max, 7);
-                break;
-            case 3:
-                playGame(min, max, 5);
-                break;
-            case 4:
-                cout << "Thanks for playing! Goodbye!\n";
-                exit(0);
-            default:
-                cout << " Invalid choice! Please select a valid option.\n";
+            } else {
+                cout << "No, " << guess << " is not the right number.\n";
+
+                if (guess > secretnumber) {
+                    cout << "The secret number is smaller than the number you chose.\n";
+                } else {
+                    cout << "The secret number is greater than the number you chose.\n";
+                }
+
+                int attemptsLeft = attempt - i - 1;
+                cout << attemptsLeft << " attempt(s) remaining.\n";
+
+                if (attemptsLeft == 0) {
+                    cout << "Oops! You couldn't find the right number. It was " << secretnumber << ".\n";
+                    cout << "Play the game again if you want!\n\n";
+                }
+            }
         }
     }
+
     return 0;
 }
